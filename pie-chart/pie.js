16da3729f76db4be88d39cbf0efc40381af7ad4c
Martin Bing <bing.martin.photography@gmail.com>
2017-11-03 16:50:54 +0100

    Add angle historgram chart example with layout

--- pie-chart/pie.js
+++ pie-chart/pie.js
@@ -6,6 +6,35 @@
                     height: 500
                 });
 
+    var basic_layout = function (data, x, y) {
+        data = data.sort(function (a, b) {
+            return d3.descending(a.value, b.value);
+        });
+
+        var colors = d3.scale.category20c(),
+            pie = d3.layout.pie()
+                            .value(function(d) {
+                                return d.value;
+                            }),
+            arc = d3.svg.arc()
+                        .outerRadius(150),
+            slice = svg.selectAll('.slice')
+                        .data(pie(data))
+                        .enter()
+                        .append('g')
+                        .attr('transform', 'translate(' + x + ', ' + y + ')');
+            
+            slice.append('path')
+                    .attr({d: arc,
+                        fill: function (d, i) {
+                            return colors(i);
+                        },
+                        title: function (d) {
+                            return d.data.label + ' (' + d.data.value + ')'; 
+                        }
+                });
+    };
+
     var manually = function (data, x, y) {
         data = data.sort(function (a, b) {
             return d3.descending(a.value, b.value);
@@ -46,11 +75,12 @@
     };
 
     d3.json('ufo-types.json', function (data) {
-        manually(data, 300, 250);
+        basic_layout(data, 200, 200);
+        manually(data, 600, 200);
 
         $('svg path').tooltip({
             container: 'body',
             placement: 'right'
         });
-    })
+    });
 })();
