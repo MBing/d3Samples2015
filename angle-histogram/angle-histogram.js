9c636d2812c2dc7c3d2e9cbdae4dba13e3949a68
Martin Bing <bing.martin.photography@gmail.com>
2017-11-04 12:29:01 +0100

    Setup and creation of layout file for histogram example

--- angle-histogram/angle-histogram.js
+++ angle-histogram/angle-histogram.js
@@ -1,63 +1,23 @@
-(function () {
-    var svg = d3.select('#graph')
-                .append('svg')
-                .style({
-                    width: '100%',
-                    height: 500
-                });
-    
-    var parseTimes = function(data) {
-        return data.map(function(d) {
-            d.time = Date.parse(d.time);
-            
-            return d;
-        }).filter(function (d) {
-            return !!d.time;
-        }).map(function (d) {
-            d.time = new Date(d.time);
-            return d;
-        });
-    }
+d3.layout.angle_histogram = function () {
+    var histogram = d3.layout.histogram(),
+        innerRadius = 0,
+        maxHeight = d3.scale.linear(),
+        minHeight = 0,
+        startAngle = 0,
+        endAngle = 2 * Math.PI;
 
-    var basic_layout = function (data, x, y) {
-        data = data.sort(function (a, b) {
-            return d3.descending(a.value, b.value);
-        });
+    function angle_histogram(data, i) {
+        var binned = histogram.call(this, data, i),
+            _startAngle = typeof startAngle === 'function' ? startAngle.apply(this, arguments) : startAngle,
+            _endAngle = typeof endAngle === 'function' ? endAngle.apply(this, arguments) : endAngle,
 
-        var colors = d3.scale.category20c(),
-            arc = d3.svg.arc(),
-            slice = svg.selectAll('.slice')
-                        .data(data)
-                        .enter()
-                        .append('g')
-                        .attr('transform', 'translate(' + x + ', ' + y + ')');
-            
-            slice.append('path')
-                    .attr({d: arc,
-                        fill: function (d, i) {
-                            return colors(i);
-                        }
-                });
-    };
+            radians = d3.scale.linear()
+                .domain([0, d3.max(binned.map(function (d) { return d.x; }))])
+                .range([_startAngle, _endAngle]);
 
-    d3.json('triangle-ufos.json', function (data) {
-        data = parseTimes(data);
-
-        var histogram = d3.layout.histogram()
-                                 .value(function (d) {
-                                     return d.time.getHours();
-                                 })
-                                 .bins(24);
-
-        var binned = histogram(data);
-
-        var radians = d3.scale.linear()
-                              .domain([0, d3.max(binned.map(function (d) { return d.x; }))])
-                              .range([0, 2 * Math.PI]),
-            innerRadius = 20;
-            
         binned = binned.map(function (d) {
-            d.innerRadius = innerRadius;
+            d.innerRadius = typeof innerRadius === 'function' ? innerRadius(d, i) : innerRadius;
+            d.value = histogram.value()(d[0]);
             d.outerRadius = d.innerRadius + d.y;
             d.startAngle = radians(d.x) - radians(d.dx / 2);
             d.endAngle = radians(d.x) + radians(d.dx / 2);
@@ -65,6 +25,80 @@
             return d;
         });
 
-        basic_layout(binned, 300, 200);
+        maxHeight.domain([typeof minHeight === 'function' ? minHeight(d3.min(binned.map(
+            function (d) {
+                return d.y;
+            }
+        ))) : minHeight
+            , d3.max(binned.map(function (d) {
+            return d.y;
+        }))]);
+
+        binned = binned.map(function (d) {
+            d.outerRadius = d.innerRadius + maxHeight(d.y);
+
+            return d;
+        })
+
+        return binned;
+    }
+
+    ['value', 'range', 'bins', 'frequency'].map(function (k) {
+        angle_histogram[k] = function () {
+            histogram[k].apply(this, arguments);
+
+            return angle_histogram;
+        };
     });
-})();
+
+    angle_histogram.innerRadius = function (x) {
+        if (!arguments.length) {
+            return innerRadius;
+        };
+
+        return angle_histogram;
+    };
+
+    angle_histogram.maxHeight = function (x) {
+        if (!arguments.length) {
+            return maxHeight;
+        };
+
+        if (typeof x === 'function') {
+            maxHeight = x;
+        } else {
+            maxHeight.range([0, x]);
+        }
+
+        return angle_histogram;
+    };
+
+    angle_histogram.minHeight = function (x) {
+        if (!arguments.length) {
+            return minHeight;
+        };
+        minHeight = x;
+
+        return angle_histogram;
+    };
+
+    angle_histogram.startAngle = function (x) {
+        if (!arguments.length) {
+            return startAngle;
+        };
+        startAngle = x;
+
+        return angle_histogram;
+    };
+
+    angle_histogram.endAngle = function (x) {
+        if (!arguments.length) {
+            return endAngle;
+        };
+        endAngle = x;
+
+        return angle_histogram;
+    };
+
+    return angle_histogram;
+}
---
+++ angle-histogram/code.js
@@ -0,0 +1,110 @@
+(function () {
+    var svg = d3.select('#graph')
+                .append('svg')
+                .style({
+                    width: '100%',
+                    height: 500
+                });
+    
+    var parseTimes = function(data) {
+        return data.map(function(d) {
+            d.time = Date.parse(d.time);
+            
+            return d;
+        }).filter(function (d) {
+            return !!d.time;
+        }).map(function (d) {
+            d.time = new Date(d.time);
+            return d;
+        });
+    }
+
+    var basicLayout = function (data, x, y) {
+        data = data.sort(function (a, b) {
+            return d3.descending(a.value, b.value);
+        });
+
+        var gradient = d3.scale.linear()
+            .range(
+                [
+                    '#00001f',
+                    '#bf4a07',
+                    '#4686dd',
+                    '#85c1f5',
+                    '#4686dd',
+                    '#ca3a22',
+                    '#00001f'
+                ]
+            )
+            .domain(
+                [
+                    3/24, 
+                    5/24, 
+                    8/24, 
+                    12/24, 
+                    18/24, 
+                    20/24, 
+                    22/24
+                ].map(
+                    d3.interpolate(0, data.length)
+                )
+            ),
+            arc = d3.svg.arc(),
+            slice = svg.selectAll('.slice')
+                        .data(data)
+                        .enter()
+                        .append('g')
+                        .attr('transform', 'translate(' + x + ', ' + y + ')');
+            
+            slice.append('path')
+                    .attr({d: arc,
+                        fill: function (d, i) {
+                            return gradient(i);
+                        }
+                });
+
+            slice.append('text')
+                .text(function (d, i) {
+                    return d.value;
+                })
+                .classed('label', true)
+                .attr({
+                    transform: function (d, i) {
+                        var angle = 180 / Math.PI * (d.startAngle + (d.endAngle - d.startAngle) / 2);
+
+                        return 'rotate(' + ( - 90 + angle) + ') translate(' + (d.innerRadius - 30) + ') rotate(90)';
+                    },
+                    'text-anchor': 'middle'
+                });
+            slice.append('line')
+                .classed('tick', true)
+                .attr({
+                    x0: 0,
+                    y0: 0,
+                    x1: 10,
+                    y1: 0,
+                    transform: function (d, i) {
+                        var angle = 180 / Math.PI * (d.startAngle + (d.endAngle - d.startAngle) / 2);
+
+                        return 'rotate(' + ( - 90 + angle) + ') translate(' + (d.innerRadius - 12) + ')';
+                    }
+                })
+    };
+
+    d3.json('triangle-ufos.json', function (data) {
+        data = parseTimes(data);
+
+        var histogram = d3.layout.angle_histogram()
+            .value(function (d) {
+                return d.time.getHours();
+            })
+            .bins(24)
+            .innerRadius(150)
+            .maxHeight(d3.scale.log().range([0, 200]))
+            .minHeight(function (min_val) {
+                return min_val / 2;
+            });
+
+        basicLayout(histogram(data), 400, 400);
+    });
+})();
